37962f5a6116078a0920923c21a830ecab44193d
Syed Rasheduddin <64092741+SyedRashed007@users.noreply.github.com>
2024-06-07 08:56:46 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1221
+let accountName = "Rashed"
+var accountState = "Hyd"
+accountAge = "25"
+
+// accountId = 2   // Cant use const to assign value again
+accountName = "Syed"
+accountState = "Mumbai"
+accountAge = "24"
+
+console.log([accountId, accountName, accountState, accountAge
+])
