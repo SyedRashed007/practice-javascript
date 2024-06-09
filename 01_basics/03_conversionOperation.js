3382f509975815206930d509a0390a754540133f
Syed Rasheduddin <64092741+SyedRashed007@users.noreply.github.com>
2024-06-09 01:50:04 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "rashed"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = "rashed"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "rashed" => true
+
+let someNumber = 27
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
