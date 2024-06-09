a172cb8514ff1ef383a9ab7d2a2e9e38e3dc35d7
Syed Rasheduddin <64092741+SyedRashed007@users.noreply.github.com>
2024-06-09 00:35:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// "use strict";
+
+// alert(3+3)
+
+let name = "Rashed"
+let age = 25
+let isLoggedIn = true
+let state
+console.log(state)
+console.log(name)
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null)
+
